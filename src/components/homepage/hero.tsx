80acd444d12d38dc1f618fe39a5ebfb0a0cb8911
ujjwal-codementor <ujjwal.codementor@gmail.com>
2025-11-20 22:30:17 +0530

    final

--- src/components/homepage/hero.tsx
+++ src/components/homepage/hero.tsx
@@ -23,7 +23,7 @@ const Hero = () => {
       <div className="container mx-auto px-6 lg:px-12 relative z-10 py-12">
         <div className="max-w-4xl mx-auto text-center">
           
-          <h1 className="text-4xl md:text-6xl lg:text-7xl font-playfair font-bold text-white mb-6 leading-tight">
+          <h1 className="text-4xl md:text-6xl lg:text-7xl font-playfair font-bold text-white mb-2 leading-tight">
             Furnish Care
           </h1>
           <h2 className="text-3xl md:text-5xl lg:text-6xl font-playfair font-bold text-white mb-6 leading-tight">
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -3,6 +3,7 @@ import { ClerkProvider } from "@clerk/nextjs"
 import type { AppProps } from "next/app"
 import GoogleAnalytics from "@/components/GoogleAnalytics"
 import Head from "next/head"
+import MetaPixel from "@/components/metaPixel"
   
 function MyApp({ Component, pageProps }: AppProps) {
   return (
@@ -10,9 +11,13 @@ function MyApp({ Component, pageProps }: AppProps) {
       {/* Global <Head> — applies favicon to all pages */}
       <Head>
         <link rel="icon" href="/Logo.jpg" />
+        <meta
+          name="keywords"
+          content="furniture touch up Dallas, furniture polish Dallas, furniture repair Dallas, wood furniture restoration Dallas, furniture maintenance subscription, furniture service plan, wood scratch repair, veneer repair, wood polishing, home furniture care, monthly furniture service, subscription furniture repair, Dallas Texas, DFW"
+        />
       </Head>
       <GoogleAnalytics />
-
+      <MetaPixel />
       <ClerkProvider
         {...pageProps}
         appearance={{
